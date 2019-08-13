993e064d1fcc8b942f6f64757a7d650c790f1a3b
Carlos Torres <carlos.a.torres-gonzalez@capgemini.com>
2019-08-13 14:42:17 +0200

    Making graphql running

--- src/main/java/com/carlos/torres/resolvers/Query.java
+++ src/main/java/com/carlos/torres/resolvers/Query.java
@@ -1,8 +1,32 @@
 package com.carlos.torres.resolvers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Query {
+import com.carlos.torres.model.impl.AuthorCto;
+import com.carlos.torres.service.AuthorService;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
+import reactor.core.publisher.Flux;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+	
+	@Autowired private AuthorService aService;
+	
+	public Query(AuthorService aService) {
+        this.aService = aService;
+    }
+	
+	public final List<AuthorCto> authors() {
+		final Flux<AuthorCto> authors = this.aService.getAuthors();
+		final List<AuthorCto> resList = new ArrayList<>();
+		if(authors != null) {
+			authors.map(author -> resList.add(author));
+		}
+		return resList;
+	}
 }
